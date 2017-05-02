456e156098d0d8a06e317d87ffc646272df0f42e
kaka <373474714@qq.com>
2017-05-03 00:44:42 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/ka/coolweatherka/db/Province.java
@@ -0,0 +1,35 @@
+package com.example.ka.coolweatherka.db;
+import org.litepal.crud.DataSupport;
+/**
+ * Created by ka on 2017/5/2.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
